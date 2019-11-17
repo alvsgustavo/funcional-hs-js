cc9558720dee864ac7c61edef7c5ffb956546a69
alvsgustavo <gustavo.daniel.alves@ccc.ufcg.edu.br>
2019-11-17 14:45:19 -0300

    javascript functions finished, lacking parser

--- javascript/app.js
+++ javascript/app.js
@@ -35,23 +35,27 @@ function netIncome (transactions, year, month) {
 };
 
 function balance (transactions, year, month) {
-  const initialBalance = transactions.filter(t => {
-    return t.data.year == year &&
-      t.data.month == month &&
-      t.data.day == 1 &&
-      t.tipos.includes('SALDO_CORRENTE');
-  })[0].valor;
-
+  const initialBalance = startingBalance(transactions, year, month);
   return initialBalance + netIncome(transactions, year, month);
 };
 
 function maximumBalance (transactions, year, month) {
-  // TODO
-
+  const dailyBalances = cashFlow(transactions, year, month);
+  const maximum = dailyBalances.reduce((acc, start) => {
+    (acc.balance > start.balance)? acc : start;
+  }, dailyBalances[0]);
+  
+  return maximum;
 };
 
 function minimumBalance (transactions, year, month) {
   // TODO
+  const dailyBalances = cashFlow(transactions, year, month);
+  const minimum = dailyBalances.reduce((acc, start) => {
+    (acc.balance < start.balance)? acc : start;
+  }, dailyBalances[0]);
+  
+  return minimum;
 };
 
 function averageIncome (transactions, year) {
@@ -76,7 +80,25 @@ function averageNetIncome (transactions, year, month) {
 };
 
 function cashFlow (transactions, year, month) {
-  // TODO
+  const thisMonth = getNetIncomeTransactionsByMonth(transactions, year, month);
+  const cashFlow = []
+  // Ensures it won't calculate a day in the future
+  const getMaxDate = {
+    (year == Date.getYear() && month == Date.getMonth()) - 1?\
+      Date.getDate() : getLastDay(year, month);
+  };
+  for (i = 0; i < getMaxDate; i++) {
+    const thisDay = getNetIncomeTransactionsByDay(thisMonth, year, month, day);
+    const dailyTransactionsValue = thisDay.reduce(defaultReducer);
+    cashFlow[i] = {day: i,
+      balance: {
+        cashFlow[i]? cashFlow[i].balance + (dailyTransactionsValue | 0)
+        \: (i == 0? initialBalance(transactions, year, month) + (dailyTransactionsValue | 0)
+        \: (dailyTransactionsValue | 0));
+      };
+    };
+  };
+  return cashFlow;
 };
 
 function getNetIncomeTransactionsByMonth (transactions, year, month, type) {
@@ -102,6 +124,18 @@ function getNetIncomeTransactionsByYear (transactions, year, type) {
   return result;
 };
 
+function getNetIncomeTransactionsByDay (transactions, year, month, day) {
+  const result = transactions.filter(t => {
+    return t.data.year == year &&
+      t.data.month == month &&
+      t.data.day == day &&
+      !t.tipos.includes('SALDO_CORRENTE') &&
+      !t.tipos.includes('APLICACAO') &&
+      !t.tipos.includes('VALOR_APLICACAO');
+  });
+  return result;
+}
+
 function isIt(type, value) {
   let result = undefined;
 
@@ -119,5 +153,19 @@ function isIt(type, value) {
       break;
   }
   return result;
+};
+
+function startingBalance (transactions, year, month) {
+  return transactions.filter(t => {
+    return t.data.year == year &&
+      t.data.month == month &&
+      t.data.day == 1 &&
+      t.tipos.includes('SALDO_CORRENTE');
+  })[0].valor;
+};
+
+function getLastDay (year, month) {
+  const date = new Date(year, month, 0);
+  return date.getDate();
 };
 
