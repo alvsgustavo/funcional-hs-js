aa47c123eeb82c4f204ebe0940405a8918612b71
alvsgustavo <gustavo.daniel.alves@ccc.ufcg.edu.br>
2019-11-18 17:34:29 -0300

    some functions in haskell

--- haskell/src/function.hs
+++ haskell/src/function.hs
@@ -29,27 +29,29 @@ data TransactionType = SALDO_CORRENTE | VALOR_APLICACAO |
                        OUTRAS_RECEITAS | DESPESAS_PESSOAL |
                        TERCEIRIZACAO_FUNCIONARIOS | VIGILANCIA |
                        SALARIO_FUNCIONARIOS_ORGANICOS | ADIANTAMENTO_SALARIAL_FUNCIONARIOS_ORGANICOS |
-                       FERIAS | INSS deriving (Eq, Show)
+                       FERIAS | INSS | APLICACAO deriving (Eq, Show)
 
 -- Filter transactions by year
 filterByYear :: [Transaction] -> Int -> [Transaction]
-filterByYear ts y = filter (\x -> (year . date) x == y) ts
+filterByYear ts y = filter (\t -> validTransaction t && checkYear t y) ts
 
 -- Filter transactions by month and year
 filterByYearMonth :: [Transaction] -> Int -> Int -> [Transaction]
-filterByYearMonth xs y m = filter (\x -> (month . date) x == m && (year . date) x == y) xs  
+filterByYearMonth ts y m = filter (\t -> validTransaction t && checkYear t y && checkMonth t m) ts
 
 -- Calculate income by month and year
 income :: [Transaction] -> Int -> Int -> Float
-income xs y m = 
+income ts y m = foldl (+) 0 $ map valor $ filterIncome
+                where filterIncome = filter (\t -> validTransaction t && checkYear t y && checkMonth t m && valor t > 0) ts
 
 -- Calculate expenses by month and year
 expenses :: [Transaction] -> Int -> Int -> Float
-expenses = undefined
+expenses ts y m = foldl (+) 0 $ map valor $ filterExpenses
+                  where filterExpenses = filter (\t -> validTransaction t && checkYear t y && checkMonth t m && valor t < 0) ts
 
 -- Calculate net income by month and year
 netIncome :: [Transaction] -> Int -> Int -> Float
-netIncome = undefined
+netIncome ts y m = foldl (+) 0 $ map valor $ filterByYearMonth ts y m
 
 -- Calculate balance by month and year
 balance :: [Transaction] -> Int -> Int -> Float
@@ -73,9 +75,23 @@ averageExpenses = undefined
 
 -- Calculate average net income by year
 averageNetIncome :: [Transaction] -> Int -> Float
-averageNetIncome = undefined
+averageNetIncome ts y = (foldl (+) 0 $ map valor $ filterByYear ts y) / 12
 
 -- Return cash flow in a specified month/year
 cashFlow :: [Transaction] -> Int -> Float
 cashFlow = undefined
 
+-- Auxiliary functions
+--
+
+checkYear :: Transaction -> Int -> Bool
+checkYear t y = (year . date) t == y
+
+checkMonth :: Transaction -> Int -> Bool
+checkMonth t m = (month . date) t == m
+
+validTransaction :: Transaction -> Bool
+validTransaction t = let saldoCorrenteCheck = SALDO_CORRENTE `elem` (tipos t)
+                         aplicacaoCheck = APLICACAO `elem` (tipos t)
+                         valorAplicacaoCheck = VALOR_APLICACAO `elem` (tipos t)
+                     in saldoCorrenteCheck && aplicacaoCheck && valorAplicacaoCheck
